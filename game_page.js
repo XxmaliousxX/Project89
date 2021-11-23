5664fd51054690df223148ae7c03e27fa134c7c8
XxmaliousxX <83023423+XxmaliousxX@users.noreply.github.com>
2021-11-22 20:30:19 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,5 +1,5 @@
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
+player1_name = localStorage.getItem("player1_name_input");
+player2_name = localStorage.getItem("player2_name_input");
 
 player1_score = 0;
 player2_score = 0;
